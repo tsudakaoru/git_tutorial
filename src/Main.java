186d8c21454e00eba618eaf317d49b980f3379b3
tsudakaoru <Kaoru.Tsuda@nttdata.com>
2026-06-08 15:23:47 +0900

    add comment

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("This is a test");
+        System.out.println("add comment");
     }
 }
