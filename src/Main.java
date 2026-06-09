b244606d05ec3829261ffb5dea493ae958ab229a
tsudakaoru <Kaoru.Tsuda@nttdata.com>
2026-06-09 10:36:24 +0900

    コンフリクトを追記

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ class Main {
         System.out.println("Hello, World!");
         System.out.println("add comment");
         System.out.println("git commit --amend");
+        System.out.println("コンフリクト");
     }
 }
